fd1a7b3a957e55d686c7b519e881c729ad58f7bd
thexcaliber <hitesh.k.83080@gmail.com>
2022-05-06 16:11:59 +0530

    CSLL

--- 2_CircularSLL.py
+++ 2_CircularSLL.py
@@ -6,14 +6,43 @@ class Node:
 class CircularSLL:
     def __init__(self):
         self.head = None
-        self.tail = None
+        self.tail = self.head
     def __iter__(self):
         node = self.head
         while node:
             yield node
             node = node.next
+            if node == self.tail.next:
+                break
             
     def insertCSLL(self,value,location):
+        if self.head == None:
+            return "The head refenence is None"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+            elif location == 1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index<location - 1:
+                    tempNode = tempNode.next
+                    index+=1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+
+                
+
+                            
+
+        
 
 
 
