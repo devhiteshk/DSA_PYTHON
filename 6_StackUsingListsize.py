f6017d3ca4f3b52e0f0cbbbc9c9c3959f1172f21
thexcaliber <hitesh.k.83080@gmail.com>
2022-05-08 18:21:00 +0530

    Stack using LL

---
+++ 6_StackUsingListsize.py
@@ -0,0 +1,62 @@
+class stack:
+    def __init__(self,maxSize):
+        self.maxSize = maxSize
+        self.list = []
+
+    def __str__(self):
+        if self.list == None:
+            return "Stack is empty"
+        else:
+            values = self.list.reverse()
+            values = [str(x) for x in self.list]
+            return "\n".join(values)
+
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+
+    def isFull(self):
+        if len(self.list) == self.maxSize:
+            return True
+        else:
+            return False
+
+    def push(self,value):
+        if self.isFull():
+            return "stack is full"
+        else:
+            self.list.append(value)
+            return "element added successfully"
+
+    def pop(self):
+        if self.list == []:
+            return "Stack is empty"
+        else:
+            return self.list.pop()
+    
+    def peek(self):
+        if self.list == []:
+            return "Stack is empty"
+        else:
+            return self.list[-1]
+
+    def delete(self):
+        self.list = None
+        return "Stack is deleted"
+
+
+    
+customStack = stack()
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+customStack.push(4)
+
+# customStack.pop()
+# print(customStack.peek())
+
+customStack.delete()
+
+print(customStack)
