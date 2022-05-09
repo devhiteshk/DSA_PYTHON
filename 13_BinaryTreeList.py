6ee89c8f280ccc8b485bd42112ebb838cf1d7a8b
thexcaliber <hitesh.k.83080@gmail.com>
2022-05-09 19:26:32 +0530

    Binary Tree using python List

--- 13_BinaryTreeList.py
+++ 13_BinaryTreeList.py
@@ -21,9 +21,54 @@ class BinaryTree:
                 return "success"
         return "Not Found"
 
-    
+    def preOrderTraversal(self,index):
+        if index > self.lastUsedIndex:
+            return
+        print(self.List[index])
+        self.preOrderTraversal(index*2)
+        self.preOrderTraversal(index*2 + 1)
+                
+    def inOrderTraversal(self,index):
+        if index > self.lastUsedIndex:
+            return
+        self.inOrderTraversal(index*2)
+        print(self.List[index])
+        self.inOrderTraversal(index*2 + 1)
+
+    def postOrderTraversal(self,index):
+        if index > self.lastUsedIndex:
+            return
+        self.postOrderTraversal(index*2)
+        self.postOrderTraversal(index*2 + 1)
+        print(self.List[index])
+
+    def levelorderTraversal(self,index):
+        for i in range(index, self.lastUsedIndex + 1):
+            print(self.List[i])
+
+    def deleteNode(self,value):
+        if self.lastUsedIndex == 0:
+            return "No node to delete"
+
+        for i in range(1,self.lastUsedIndex + 1):
+            if self.List[i] == value:
+                self.List[i] = self.List[self.lastUsedIndex]
+                self.List[self.lastUsedIndex] = None
+                self.lastUsedIndex -= 1
+                return "Node Deleted"
+
+    def deleteBT(self):
+        self.List = None
+        return "Binary Tree Deleted"
+
 newBT = BinaryTree(8)
 newBT.insertNode('Drinks')
 newBT.insertNode('Hot')
 newBT.insertNode('Cold')
 print(newBT.List)
+
+# newBT.preOrderTraversal(1)
+# newBT.inOrderTraversal(1)
+# newBT.postOrderTraversal(1)
+newBT.deleteNode('Hot')
+newBT.levelorderTraversal(1)
