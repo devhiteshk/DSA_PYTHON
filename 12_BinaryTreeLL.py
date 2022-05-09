40d52f58087c3316565b4710104e9b54eaeac835
thexcaliber <hitesh.k.83080@gmail.com>
2022-05-09 18:25:00 +0530

    Binary Tree using LL

--- 12_BinaryTreeLL.py
+++ 12_BinaryTreeLL.py
@@ -111,6 +111,93 @@ def searchBT(rootNode, nodeValue):
                 myQueue.enqueue(root.value.rightChild)
         return "not found"
  
+
+def insertNodeBT(rootNode, newNode):
+    if not rootNode:
+        rootNode = newNode
+    else:
+        myQueue = Queue()
+        myQueue.enqueue(rootNode)
+        while not(myQueue.isEmpty()):
+            root = myQueue.dequeue()
+            if root.value.leftChild is not None:
+                myQueue.enqueue(root.value.leftChild)
+            else:
+                root.value.leftChild = newNode
+                return "successfully inserted"
+            if root.value.rightChild is not None:
+                myQueue.enqueue(root.value.rightChild)
+            else:
+                root.value.rightChild = newNode
+                return "successfully inserted"
+
+
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+    else:
+        myQueue = Queue()
+        myQueue.enqueue(rootNode)
+        while not(myQueue.isEmpty()):
+            root = myQueue.dequeue()
+            if root.value.leftChild is not None:
+                myQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                myQueue.enqueue(root.value.rightChild)
+        deepestNode = root.value
+        return deepestNode
+
+def deleteDeepestNode(rootNode, dNode):
+    if not rootNode:
+        return
+    else:
+        myQueue = Queue()
+        myQueue.enqueue(rootNode)
+        while not(myQueue.isEmpty()):
+            root = myQueue.dequeue()
+            if root.value == dNode:
+                root.value = None
+                return
+            if root.value.rightChild:
+                if root.value.rightChild is dNode:
+                    root.value.rightChild = None
+                    return
+                else:
+                    myQueue.enqueue(root.value.rightChild)
+            if root.value.leftChild:
+                if root.value.leftChild is dNode:
+                    root.value.leftChild = None
+                    return
+                else:
+                    myQueue.enqueue(root.value.leftChild)
+
+def deleteNodeBT(rootNode, node):
+    if not rootNode:
+        return
+    else:
+        myQueue = Queue()
+        myQueue.enqueue(rootNode)
+        while not(myQueue.isEmpty()):
+            root = myQueue.dequeue()
+            if root.value.data == node:
+                dNode = getDeepestNode(rootNode)
+                root.value.data = dNode.data
+                deleteDeepestNode(rootNode, dNode)
+                return "Node deleted successfully"
+            if root.value.leftChild is not None:
+                myQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                myQueue.enqueue(root.value.rightChild)
+        return "Failed to delete"
+
+
+def deleteBT(rootNode):
+    rootNode.data = None
+    rootNode.leftChild = None
+    rootNode.rightChild = None
+    return "complete BT deleted"
+
+
 newBT = TreeNode("Drinks")
 leftC = TreeNode("Hot")
 rightC = TreeNode("Cold")
@@ -122,4 +209,9 @@ leftC.leftChild = lleftC
 leftC.rightChild = lrightC
 
 levelOrderTraversal(newBT)
-print(searchBT(newBT,"Ta"))
+
+deleteNodeBT(newBT, "Tea")
+
+levelOrderTraversal(newBT)
+# print(searchBT(newBT,"Ta"))
+# print(getDeepestNode(newBT))
