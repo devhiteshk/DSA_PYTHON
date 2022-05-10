085688038c65e2451ab396c14a74a95546f85301
thexcaliber <hitesh.k.83080@gmail.com>
2022-05-10 18:48:29 +0530

    Insertion Sort

---
+++ 19_InsertionSort.py
@@ -0,0 +1,14 @@
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j>=0 and key <arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+arr = [10,2,4,6,2,1,8,7]
+
+print(insertionSort(arr))
+
